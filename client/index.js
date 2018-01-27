2f4c9ba7f92b955c0df70d95468b3dc464416e2f
Justin <jvcruz@ucdavis.edu>
2018-01-26 21:24:24 -0800

    inputs are sanitized

--- client/index.js
+++ client/index.js
@@ -1,10 +1,32 @@
 const errorMessage = document.getElementById('error-message');
 
+
+const isValid = input => {
+  return input.every(e => typeof e === 'number');
+};
+
+const handleError = (errorMessage, object) => {
+  object.value = '';
+  errorMessage.textContent = 'Please enter a valid search';
+};
 const handler = ({ key, target }, object) => {
   if(key !== 'Enter') return;
-  const params = target.value;
+  let input;
+  
+  try {
+  	input = JSON.parse(`[${target.value}]`);
+  } catch(error) {
+  	handleError(errorMessage, object);
+  	return;
+  }
 
-  let walls = axios.get(`/api/${target.value}`)
+  if(!isValid(input)) {
+		handleError(errorMessage, object);
+  	return;  	
+  }
 
+  let walls = axios.get(`/api/${input}`)
+
+  errorMessage.textContent = '';
   object.value = '';
 };
