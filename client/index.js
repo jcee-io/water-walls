40fd405486ce807609e6852ab3941c3fef58d0b5
Justin <jvcruz@ucdavis.edu>
2018-01-27 00:28:36 -0800

    block map created

--- client/index.js
+++ client/index.js
@@ -10,6 +10,33 @@ const handleError = (errorMessage, object) => {
   errorMessage.textContent = 'Please enter a valid search';
 };
 
+
+const createMap = walls => {
+	const map = [];
+	const wallArr = Object.keys(walls).map(index => walls[index]);
+	const largestHeight = wallArr.reduce((acc, wall) => Math.max(acc, wall.wallHeight), walls[0].wallHeight);
+
+	for(let i = 0; i < Object.keys(walls).length; i++){
+		let { waterHeight, wallHeight } = walls[i];
+		let temp = [];
+		for(let j = 0; j < largestHeight; j++) {
+			if(wallHeight > 0) {
+				temp.push('wall');
+				wallHeight--;
+			} else if (waterHeight > 0) {
+				temp.push('water');
+				waterHeight--;
+			} else {
+				temp.push('air');
+			}
+		}
+		map.push(temp);
+	}
+
+	console.log(largestHeight);
+	return map;
+};
+
 const handler = ({ key, target }, object) => {
   if(key !== 'Enter') return;
   let params;
@@ -27,6 +54,7 @@ const handler = ({ key, target }, object) => {
   }
 
   axios.get(`/api`, { params })
+    .then(({ data }) => createMap(data))
     .then(console.log);
 
   errorMessage.textContent = '';
